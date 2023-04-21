51775919ea80ad3614a372a8bb97d88a41d446d0
shinzz410 <tlswldnjs410@gmail.com>
2023-04-21 13:54:53 +0900

    2차 과제

---
+++ sopt_2nd_week_practice/sopt_2nd_week_practice/Extension.swift
@@ -0,0 +1,85 @@
+//
+//  Extension.swift
+//  sopt_2nd_week_practice
+//
+//  Created by 신지원 on 2023/04/20.
+//
+
+import UIKit
+
+extension UIButton {
+    func setUnderLine() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count))
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 22, height: 14))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+}
+
+extension UITextField {
+    //placeholder 색 변경
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
+    }
+}
+
+extension FirstViewController {
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField.hasText || textField.isEditing {
+            textField.layer.borderWidth = 1
+            textField.layer.borderColor = UIColor.tvingred.cgColor
+            
+            if(textField == passwordtextfield) {
+                eyeButton.isHidden = false
+                clearButton.isHidden = false
+                clearButton.isEnabled = true
+            }
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        textField.layer.borderWidth = 0
+        
+        //왜 바로 안되고 한 텀 나중에 빨개지는지 모르겠어요,,
+        if idtextfield.hasText && passwordtextfield.hasText {
+            loginButton.isEnabled = true
+            loginButton.layer.borderWidth = 0
+            loginButton.layer.backgroundColor = UIColor.tvingred.cgColor
+            loginButton.setTitleColor(.white, for: .normal)
+        }
+        else {
+            loginButton.isEnabled = false
+            loginButton.layer.backgroundColor = UIColor.black.cgColor
+            loginButton.setTitleColor(.tvinggray2, for: .normal)
+            loginButton.layer.borderWidth = 1
+        }
+        
+    
+    }
+}
+
+
+
+
---
+++ sopt_2nd_week_practice/sopt_2nd_week_practice/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  sopt_2nd_week_practice
+//
+//  Created by 신지원 on 2023/04/19.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tvingred = UIColor(red: 255/255, green: 20/255, blue: 60/255, alpha: 1.0)
+    static let tvinggray1 = UIColor(red: 214/255, green: 214/255, blue: 214/255, alpha: 1.0)
+    static let tvinggray2 = UIColor(red: 156/255, green: 156/255, blue: 156/255, alpha: 1.0)
+    static let tvinggray3 = UIColor(red: 98/255, green: 98/255, blue: 98/255, alpha: 1.0)
+    static let tvinggray4 = UIColor(red: 46/255, green: 46/255, blue: 46/255, alpha: 1.0)
+}
