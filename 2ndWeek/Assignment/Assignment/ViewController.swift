27f3fd5c132c3c8e44c2b188c712b11a8ccd55fd
shinzz410 <tlswldnjs410@gmail.com>
2023-07-01 03:15:13 +0900

    초기세팅

---
+++ 2ndWeek/Assignment/Assignment/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by 신지원 on 2023/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setLayout()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setStyle() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+
+
+}
+
