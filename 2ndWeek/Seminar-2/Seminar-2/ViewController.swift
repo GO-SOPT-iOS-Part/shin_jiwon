c7e731b7572c47b92061b6234fb2ea913a3f1e36
shinzz410 <tlswldnjs410@gmail.com>
2023-07-01 00:59:52 +0900

    UIComponents 세팅

--- 2ndWeek/Seminar-2/Seminar-2/ViewController.swift
+++ 2ndWeek/Seminar-2/Seminar-2/ViewController.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
+    
+    let firstView = UIView()
+    let secondView = UIView()
+    let thirdView = UIView()
+    let fourthView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setStyle()
+        setLayout()
         // Do any additional setup after loading the view.
+    }
+    
+    func setStyle() {
+        [firstView, secondView, thirdView, fourthView]
+            .forEach{ view.addSubview($0) }
+        
+        view.backgroundColor = .white
+        
+        firstView.do {
+            $0.backgroundColor = .green
+        }
+        secondView.do {
+            $0.backgroundColor = .green
+        }
+        thirdView.do {
+            $0.backgroundColor = .black
+        }
+        fourthView.do {
+            $0.backgroundColor = .blue
+        }
+    }
+    
+    func setLayout() {
+        
+        
     }
 
 
