00c4df6f4e0c7d823f496d841f3cff038a751179
shinzz410 <tlswldnjs410@gmail.com>
2023-06-30 18:30:46 +0900

    초기세팅

---
+++ 2ndWeek/Seminar-2/Seminar-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar-2
+//
+//  Created by 신지원 on 2023/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
