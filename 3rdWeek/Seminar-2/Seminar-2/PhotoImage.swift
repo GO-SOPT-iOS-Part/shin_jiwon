fc4984a381f7edfb2a2a83ed26b93a0c84fb412b
shinzz410 <tlswldnjs410@gmail.com>
2023-08-01 15:57:48 +0900

    [Feat] 초기 세팅

---
+++ 3rdWeek/Seminar-2/Seminar-2/PhotoImage.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoImage.swift
+//  Seminar-2
+//
+//  Created by 신지원 on 2023/08/01.
+//
+
+import Foundation
---
+++ 3rdWeek/Seminar-2/Seminar-2/PhotoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seminar-2
+//
+//  Created by 신지원 on 2023/08/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class PhotoViewController: UIViewController {
+
+    private lazy var collectionView = UICollectionView(frame: .zero,
+                                                      collectionViewLayout: UICollectionViewFlowLayout())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
