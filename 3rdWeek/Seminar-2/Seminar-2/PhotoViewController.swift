f846ec72b13b50cc781ef94e4b00cea71f14d060
shinzz410 <tlswldnjs410@gmail.com>
2023-08-01 15:59:14 +0900

    [Feat] heart 버튼false 로 설정

--- 3rdWeek/Seminar-2/Seminar-2/PhotoViewController.swift
+++ 3rdWeek/Seminar-2/Seminar-2/PhotoViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 import SnapKit
 import Then
 
+struct Photo {
+    let image: UIImage
+    var heartTapped: Bool
+}
+
+extension Photo {
+    static func dummy() -> [Photo] {
+        var photos: [Photo] = []
+        for number in 1...18 {
+            photos.append(Photo(image: UIImage(named: "photo_\(number)")!,
+                                heartTapped: false))
+        }
+        return photos + photos
+    }
+}
+
 class PhotoViewController: UIViewController {
 
     private lazy var collectionView = UICollectionView(frame: .zero,
