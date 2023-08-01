320b9a3e2a1e0b06a0d300f0054038360a0ef39b
shinzz410 <tlswldnjs410@gmail.com>
2023-08-02 01:40:23 +0900

    [Feat] 초기 세팅

---
+++ 3rdWeek/Assignment/Assignment/MypageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MypageViewController.swift
+//  Assignment
+//
+//  Created by 신지원 on 2023/08/02.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class MypageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setLayout()
+    }
+
+    private func setStyle() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+
+}
