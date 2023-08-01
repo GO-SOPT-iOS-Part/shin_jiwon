933cfae76128337508f9cdee2a51bdfd8843ae67
shinzz410 <tlswldnjs410@gmail.com>
2023-08-01 15:58:18 +0900

    [Feat] 초기 세팅

--- 3rdWeek/Seminar-2/Seminar-2/PhotoImage.swift
+++ 3rdWeek/Seminar-2/Seminar-2/PhotoImage.swift
@@ -5,4 +5,26 @@
 //  Created by 신지원 on 2023/08/01.
 //
 
-import Foundation
+import UIKit.UIImage
+
+extension UIImage {
+
+    static var photo_1: UIImage { UIImage(named: "photo_1")! }
+    static var photo_2: UIImage { UIImage(named: "photo_2")! }
+    static var photo_3: UIImage { UIImage(named: "photo_3")! }
+    static var photo_4: UIImage { UIImage(named: "photo_4")! }
+    static var photo_5: UIImage { UIImage(named: "photo_5")! }
+    static var photo_6: UIImage { UIImage(named: "photo_6")! }
+    static var photo_7: UIImage { UIImage(named: "photo_7")! }
+    static var photo_8: UIImage { UIImage(named: "photo_8")! }
+    static var photo_9: UIImage { UIImage(named: "photo_9")! }
+    static var photo_10: UIImage { UIImage(named: "photo_10")! }
+    static var photo_11: UIImage { UIImage(named: "photo_11")! }
+    static var photo_12: UIImage { UIImage(named: "photo_12")! }
+    static var photo_13: UIImage { UIImage(named: "photo_13")! }
+    static var photo_14: UIImage { UIImage(named: "photo_14")! }
+    static var photo_15: UIImage { UIImage(named: "photo_15")! }
+    static var photo_16: UIImage { UIImage(named: "photo_16")! }
+    static var photo_17: UIImage { UIImage(named: "photo_17")! }
+    static var photo_18: UIImage { UIImage(named: "photo_18")! }
+}
