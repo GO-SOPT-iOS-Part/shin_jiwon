af830e2fe26ee68de70242d1f46a6102efba6986
shinzz410 <tlswldnjs410@gmail.com>
2023-08-01 14:43:11 +0900

    [Fix] BaseVC & BaseView 구현

---
+++ 3rdWeek/Seminar/Seminar/CarrotViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar
+//
+//  Created by 신지원 on 2023/08/01.
+//
+
+import UIKit
+
+class CarrotViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
