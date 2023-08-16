f0dce26fce52114df76774b19c175806f0c42595
shinzz410 <tlswldnjs410@gmail.com>
2023-08-17 04:29:53 +0900

    [Refactor] Network Response 불필요한 변수 삭제

--- 4thWeek/Assignment/Assignment/Network/MovieResponse.swift
+++ 4thWeek/Assignment/Assignment/Network/MovieResponse.swift
@@ -8,31 +8,10 @@
 import Foundation
 
 struct MovieResponse: Codable {
-    let page: Int
     let results: [Results]
-    let dates: Rates
-    let total_pages: Int
-    let total_results: Int
 }
 
 struct Results: Codable {
     let poster_path: String?
-    let adult: Bool
-    let overview: String
-    let release_date: String
-    let genre_ids: [Int]
-    let id: Int
-    let original_title: String
-    let original_language: String
     let title: String
-    let backdrop_path: String?
-    let popularity: Double
-    let vote_count: Int
-    let video: Bool
-    let vote_average: Double
-}
-
-struct Rates: Codable {
-    let maximum: String
-    let minimum: String
 }
